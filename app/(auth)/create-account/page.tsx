33ec7e27bcacb0599515cd5e78cd94f9ae5fd828
맥동우 <t6u80o@naver.com>
2025-03-12 18:17:38 +0900

    feat : page link

--- app/(auth)/create-account/page.tsx
+++ app/(auth)/create-account/page.tsx
@@ -1,5 +1,6 @@
 import CreateAccountForm from "@/components/form/create-account";
 import Image from "next/image";
+import Link from "next/link";
 
 export const metadata = {
   title: "회원가입",
@@ -9,10 +10,18 @@ export default function CreateAccount() {
   return (
     <div className="relative w-full min-h-screen flex justify-center items-center p-5">
       <div className="p-10 px-14 w-2xl mx-auto bg-white/90 rounded-2xl">
-        <h1 className="font-extrabold text-2xl mb-10 uppercase">
+        <h1 className="font-ubuntu font-extrabold text-2xl mb-10 uppercase">
           create account
         </h1>
         <CreateAccountForm />
+        <p className="mt-5 text-center">
+          <Link
+            href="/login"
+            className="font-semibold text-blue-600 hover:text-blue-400 transition-colors"
+          >
+            로그인 페이지로 이동
+          </Link>
+        </p>
       </div>
       <Image
         src="/slash.gif"
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 import LoginForm from "@/components/form/login-form";
 import Image from "next/image";
+import Link from "next/link";
 
 export const metadata = {
   title: "로그인",
@@ -8,11 +9,22 @@ export const metadata = {
 export default function LoginPage() {
   return (
     <div className="relative w-full min-h-screen flex justify-center items-center p-5">
-      <div className="p-10 px-14 w-2xl mx-auto bg-white/80 rounded-2xl">
-        <h1 className="font-extrabold text-2xl mb-10 uppercase">
+      <div className="w-2xl p-10 px-14 mx-auto bg-white/80 rounded-2xl">
+        <h1 className="font-extrabold text-2xl uppercase mb-10">
           welcome back
         </h1>
         <LoginForm />
+        <p className="mt-5 text-center">
+          <span className="text-neutral-700 mr-2">
+            회원가입을 하지 않았다면?
+          </span>
+          <Link
+            href="/create-account"
+            className="font-semibold text-blue-600 hover:text-blue-400 transition-colors"
+          >
+            회원가입 페이지로 이동
+          </Link>
+        </p>
       </div>
       <Image
         src="/slash.gif"
