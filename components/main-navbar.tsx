6d94f2bce5a94cc4e744a20c5548e15edd7c5548
맥동우 <t6u80o@naver.com>
2025-03-14 18:13:07 +0900

    refactor : 메뉴 컴포넌트 분리

--- components/main-navbar.tsx
+++ components/main-navbar.tsx
@@ -3,33 +3,19 @@
 import Link from "next/link";
 import Tab from "./main-navbar-tab";
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { usePathname } from "next/navigation";
 
 export default function MainNavbar() {
   const pathname = usePathname();
 
   const [pathnameState, setPathnameState] = useState(pathname);
-
   const [isMenuShown, setIsMenuShown] = useState(false);
-  const [isScroll, setIsScroll] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuShown((pre) => !pre);
   };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScroll(window.scrollY > 15);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   useEffect(() => {
     if (pathname !== pathnameState && isMenuShown) {
@@ -40,41 +26,75 @@ export default function MainNavbar() {
 
   return (
     <div className="sticky top-0 left-0 w-full max-h-screen z-50">
-      {/* mobile content */}
-      <div className="lg:hidden block bg-neutral-100/80 dark:bg-neutral-800/80 p-2 px-4">
-        <div className="grid grid-cols-3 items-center">
-          <p
-            className="cursor-pointer hover:text-neutral-500 transition-colors *:size-7"
-            onClick={toggleMenu}
-          >
-            {isMenuShown ? <XMarkIcon /> : <Bars3Icon />}
-          </p>
-          <Link href="/" className="text-2xl font-extrabold text-center">
-            夢遊
-          </Link>
-          <div className="text-end" />
-        </div>
+      <MobileContent isMenuShown={isMenuShown} toggleMenu={toggleMenu} />
+      {/* menu bar */}
+      <Menu pathname={pathname} isMenuShown={isMenuShown} />
+    </div>
+  );
+}
+
+interface MobileContentProps {
+  isMenuShown: boolean;
+  toggleMenu: () => void;
+}
+
+function MobileContent({ isMenuShown, toggleMenu }: MobileContentProps) {
+  return (
+    <div className="lg:hidden block bg-neutral-100/80 dark:bg-neutral-800/80 p-2 px-4 relative">
+      <div className="grid grid-cols-3 items-center">
+        <p
+          className="cursor-pointer hover:text-neutral-500 transition-colors *:size-7"
+          onClick={toggleMenu}
+        >
+          {isMenuShown ? <XMarkIcon /> : <Bars3Icon />}
+        </p>
+        <Link href="/" className="text-2xl font-extrabold text-center">
+          夢遊
+        </Link>
+        <div className="text-end" />
       </div>
-      {/* desktop menu content */}
-      <div
-        className={`lg:block p-5 w-full h-full lg:min-h-screen bg-white dark:bg-neutral-950 transition-all 
-          dark:shadow-neutral-500 shadow lg:shadow-none ${
-            isMenuShown ? "" : "hidden"
-          }`}
-      >
-        <div className="grid lg:grid-rows-4 grid-rows-3 gap-3 items-center h-full">
-          <Tab tabName="service" pathname={pathname} />
-          <Tab tabName="profile" pathname={pathname} />
-          <Tab tabName="about" pathname={pathname} />
-          <Link
-            href="/"
-            className={`${
-              isScroll ? "text-7xl" : "text-8xl"
-            } font-extrabold transition-all duration-800 mt-auto hidden lg:block`}
-          >
-            夢遊
-          </Link>
-        </div>
+    </div>
+  );
+}
+
+interface MenuProps {
+  pathname: string;
+  isMenuShown: boolean;
+}
+function Menu({ pathname, isMenuShown }: MenuProps) {
+  const [isScroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScroll(window.scrollY > 20);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`p-5 w-full h-full bg-white dark:bg-neutral-950 transition-all lg:block
+    dark:shadow-neutral-500 lg:shadow-none shadow top-0 ${
+      isMenuShown ? "block" : "hidden"
+    }`}
+    >
+      <div className="grid lg:grid-rows-4 grid-rows-3 gap-3 items-center h-full">
+        <Tab tabName="service" pathname={pathname} />
+        <Tab tabName="profile" pathname={pathname} />
+        <Tab tabName="about" pathname={pathname} />
+        <Link
+          href="/"
+          className={`${
+            isScroll ? "text-7xl" : "text-8xl"
+          } font-extrabold transition-all duration-800 mt-auto hidden lg:block`}
+        >
+          夢遊
+        </Link>
       </div>
     </div>
   );
