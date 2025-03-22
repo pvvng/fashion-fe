89137d90a792b82fa8b578e5c35c8765a9852385
맥동우 <t6u80o@naver.com>
2025-03-22 14:16:59 +0900

    design : link 태그로 감싸기

--- components/write-link-button.tsx
+++ components/write-link-button.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function WriteLinkButton({ link }: { link: string }) {
   return (
-    <div
-      className="fixed bottom-20 right-8 size-14 rounded-full flex justify-center items-center transition-colors shadow-md
-  bg-blue-700 hover:bg-blue-600 text-white z-100"
-    >
-      <Link href={link}>
+    <Link href={link}>
+      <div
+        className="fixed bottom-20 right-8 size-14 rounded-full flex justify-center items-center transition-colors shadow-md
+      bg-blue-700 hover:bg-blue-600 text-white z-100"
+      >
         <PencilSquareIcon className="size-6" />
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
